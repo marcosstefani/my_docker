26fabaaa2601b1df790ace232f216f5b320a1f9f
Marcos Rosa <marcos.rosa@mindera.com>
2021-07-12 21:37:47 -0300

    first detail window

--- main.py
+++ main.py
@@ -1,44 +1,58 @@
-import sys, time
-
-from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu
+import sys, docker, threading
 from PyQt5.QtGui import QIcon
 
-import docker
+from PyQt5.QtWidgets import QApplication, QListWidget, QMainWindow, QMenu, QMessageBox, QSystemTrayIcon, qApp
 
-class ContainerItem:
-    def __init__(self, container):
-        self.icon = QIcon('on.png' if container.status == 'running' else 'off.png')
-        self.name = container.name
+class TrayIcon(QSystemTrayIcon):
+    def __init__(self, app):
+        super(TrayIcon, self).__init__(QIcon('icon.png'), parent=app)
+        self.setToolTip("My Docker")
 
-class Menu:
-    def __init__(self, containers):
+class Details(QMainWindow):
+    def __init__(self):
+        super(Details, self).__init__()
+        self.setWindowTitle("My Docker")
+        self.setGeometry(300, 300, 640, 480)
+        self.list = ContainerList()
+        self.setCentralWidget(self.list)
+        self.load_list()
+        self.list.itemClicked.connect(self.list.clicked)
+
+    def load_list(self):
+        docker = Docker()
+        self.list.clear()
+        for container in docker.containers:
+            self.list.addItem(container.name)
+        threading.Timer(5.0, self.load_list).start()
+
+class ContainerList(QListWidget):
+    def clicked(self, item):
+        QMessageBox.information(self, "Aviso", "Você clicou em " + item.text())
+
+class Docker:
+    def __init__(self):
+        self.client = docker.from_env()
+        self.containers = self.client.containers.list(all=False)
+
+class Application:
+    def __init__(self):
+        app = QApplication(sys.argv)
+        QApplication.setQuitOnLastWindowClosed(False)
+        self.trayIcon = TrayIcon(app)
         self.menu = QMenu()
-        self.containers = containers
-        self.load()
+        self.trayIcon.setContextMenu(self.menu)
+        self.details_action = self.menu.addAction('Details')
+        self.details_action.triggered.connect(self.show_details)
+        self.exit_action = self.menu.addAction("Exit")
+        self.exit_action.triggered.connect(qApp.quit)
 
-    def load(self):
-        self.menu.clear()
-        for container in self.containers:
-            item = ContainerItem(container)
-            self.menu.addAction(item.icon, item.name)
-        
-        self.menu.addSeparator()
+        self.trayIcon.show()
+        sys.exit(app.exec_())
+
+    def show_details(self):
+        self.details = Details()
+        self.details.show()
 
 
-        updateAction = self.menu.addAction("Update")
-        updateAction.triggered.connect(self.load)
-
-        exitAction = self.menu.addAction("Close")
-        exitAction.triggered.connect(app.quit)
-
-app = QApplication(sys.argv)
-client = docker.from_env()
-
-trayIcon = QSystemTrayIcon(QIcon('icon.png'), parent=app)
-trayIcon.setToolTip('My Docker')
-trayIcon.show()
-
-menu = Menu(client.containers.list(all=True)).menu
-
-trayIcon.setContextMenu(menu)
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    Application()
